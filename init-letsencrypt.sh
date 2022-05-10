35bd5ae174bfcad31806f6ddd51250c0ca9fde7c
zhtcks <zhtcks@gmail.com>
2022-05-11 01:19:56 +0900

    refactor: 도메인 교체

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=cotchan.net
+domains=dev-cotchan.com
 rsa_key_size=4096
 data_path="./data/certbot"
 email="zhtcks@gmail.com" # Adding a valid address is strongly recommended
@@ -30,7 +30,6 @@ fi
 echo "### Creating dummy certificate for $domains ..."
 path="/etc/letsencrypt/live/$domains"
 mkdir -p "$data_path/conf/live/$domains"
-cd "/home/ubuntu/srv/ubuntu/"
 docker-compose run --rm --entrypoint "\
   openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
     -keyout '$path/privkey.pem' \
@@ -44,7 +43,6 @@ docker-compose up --force-recreate -d nginx
 echo
 
 echo "### Deleting dummy certificate for $domains ..."
-cd "/home/ubuntu/srv/ubuntu/"
 docker-compose run --rm --entrypoint "\
   rm -Rf /etc/letsencrypt/live/$domains && \
   rm -Rf /etc/letsencrypt/archive/$domains && \
@@ -68,7 +66,6 @@ esac
 # Enable staging mode if needed
 if [ $staging != "0" ]; then staging_arg="--staging"; fi
 
-cd "/home/ubuntu/srv/ubuntu/"
 docker-compose run --rm --entrypoint "\
   certbot certonly --webroot -w /var/www/certbot \
     $staging_arg \
